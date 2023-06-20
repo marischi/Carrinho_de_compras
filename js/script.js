ccceeca45470d021feb1c23afd25dfda84491498
marischi <marileneifsp@gmail.com>
2023-06-20 11:33:17 -0300

    carrinho

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let modalQt = 1;
+let cart = [];
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -19,6 +21,7 @@ modeloJson.map((item, index) => {
 
         let key = e.target.closest('.modelo-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
         
         c('.modeloBig img').src = modeloJson[key].img;
         c('.modeloInfo h1').innerHTML = modeloJson[key].name;
@@ -82,6 +85,48 @@ cs('.modeloInfo-size').forEach((size, sizeIndex)=>{
         size.classList.add('selected');
     });
 });
+
+//adicionar ao carrinho
+c('.modeloInfo-addButton').addEventListener('click', () => {
+    //reunir as informações: modelo, tamanho, qtd
+    let size = c('.modeloInfo-size.selected').getAttribute('data-key');
+
+    //juntar id e tamanho
+    let identifier = modeloJson[modalKey].id + '/' + size;
+
+    //verificar se já está no carrinho
+    let key = cart.findIndex((item) => {
+        return item.identifier == identifier;
+    });
+
+    //verificar se é igual ou não
+    if(key > -1){
+        cart[key].qt += modalQt;
+    }else{
+    //adicionar ao carrinho
+        cart.push({
+            identifier,
+            id: modeloJson[modalKey].id,
+            size,
+            qt: modalQt,
+        });
+    }   
+
+    updateCart();
+    closeModal();
+});
+
+//Atualizar carrinho
+function updateCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+    }else{
+        c('aside').classList.remove('show');
+    }
+}
+
+
+
 
 
 
